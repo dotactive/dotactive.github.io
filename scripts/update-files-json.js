4ed17a02ce1db94d3641ab28e9c8ea368dafc465
pinktitrud <rudy@samsgroup.com.au>
2023-07-22 22:11:22 +1000

    update js and yml

--- scripts/update-files-json.js
+++ scripts/update-files-json.js
@@ -1,11 +1,25 @@
 const fs = require('fs');
-const path = require('path');
 
-const directoryPath = './'; // Change this to the directory path where your text files are located
+async function updateFilesJson() {
+  try {
+    // Fetch the list of text files from the other repository (buried-seed)
+    const response = await fetch('https://api.github.com/repos/pinktit/buried-seed/contents/');
+    const data = await response.json();
 
-const files = fs.readdirSync(directoryPath).filter(file => file.endsWith('.txt'));
+    // Extract the file names from the response
+    const fileNames = data.map((file) => file.name).filter((name) => name.endsWith('.txt'));
 
-const json = JSON.stringify(files);
+    // Create the JSON object
+    const filesJson = { files: fileNames };
 
-fs.writeFileSync('files.json', json);
-console.log('files.json updated successfully.');
+    // Write the JSON object to files.json
+    fs.writeFileSync('files.json', JSON.stringify(filesJson, null, 2));
+
+    console.log('files.json updated successfully.');
+  } catch (error) {
+    console.error('Error updating files.json:', error.message);
+    process.exit(1);
+  }
+}
+
+updateFilesJson();
