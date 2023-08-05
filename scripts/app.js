6f9431255accb40015fe34ec258ad4984545b24d
pinktitrud <rudy@samsgroup.com.au>
2023-08-05 22:46:39 +1000

    tem

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,4 @@
-import { createApp } from 'vue';
+import { createApp } from 'https://unpkg.com/vue@3/dist/vue.global.prod.js';
 import NovelViewer from 'https://pinktit.github.io/scripts/novelViewer.vue'; 
 
 const app = createApp({});
