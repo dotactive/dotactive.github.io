9861ff6e956ac986c71cf1af20e444db3e057e8c
pinktitrud <rudy@samsgroup.com.au>
2023-08-05 21:07:35 +1000

    trying template

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,4 @@
-import { createApp } from 'vue';
+import { createApp } from 'https://unpkg.com/vue@3/dist/vue.global.prod.js';
 import NovelViewer from './novelViewer.vue';
 
 const app = createApp({
