c3d79d4bfe6f1b4725b53b468b82194daedfdd2b
pinktitrud <rudy@samsgroup.com.au>
2023-08-29 21:01:07 +1000

    remove and temp

--- scripts/app.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'https://unpkg.com/vue@3/dist/vue.global.prod.js';
-import NovelViewer from 'https://pinktit.github.io/scripts/novelViewer.vue'; 
-
-const app = createApp({});
-app.component('novel-viewer', NovelViewer);
-
-app.mount('#app');
