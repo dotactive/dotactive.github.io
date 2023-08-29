04c47c42c503ff53be86c1846b3f14e12821992f
pinktit <47837240+pinktit@users.noreply.github.com>
2023-08-29 13:42:24 +1000

    Create loadhead.js

---
+++ scripts/loadhead.js
@@ -0,0 +1,7 @@
+fetch('./head.html')
+  .then(response => response.text())
+  .then(data => {
+    const headElement = document.querySelector('head');
+    headElement.insertAdjacentHTML('beforeend', data);
+  })
+  .catch(error => console.error('Error loading head.html:', error));
