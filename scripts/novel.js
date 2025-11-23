390104e1d9589e271c96f76e689e8405b1ffdd86
pinktitrud <rudy@samsgroup.com.au>
2025-11-23 16:50:35 +1100

    cover image

--- scripts/novel.js
+++ scripts/novel.js
@@ -35,7 +35,7 @@ myapp.component('novel-viewer', {
     <button @click="hideMenu" class="menu-close"><i class="fa fa-solid fa-times transform hover:rotate-12"></i></button>
     <ul id="novel-list" class="mt-12">
       <!-- Add a list item for the cover image -->
-      <li v-if="coverImage" key="cover">
+ <li v-if="coverImage" key="cover">
         <img :src="coverImage" alt="Cover" @click="scrollToSection('cover')" class="w-20 h-auto cursor-pointer">
       </li>
       <li v-for="fileName in files" :key="fileName">
@@ -45,8 +45,19 @@ myapp.component('novel-viewer', {
   </div>
   <div class="menu-mask" @click="hideMenu"></div>
   <div id="novel-content" class="container mx-auto novel-content p-10">
-    <!-- Add an image element for the cover image -->
-    <img v-if="coverImage" :src="coverImage" alt="Cover" id="cover" class="w-1/2 h-auto pt-32  mx-auto">
+    <!-- 3D Book Cover -->
+    <div v-if="coverImage" id="cover" class="pt-32 pb-20" style="perspective: 1500px;">
+      <div class="book-3d" style="width: 300px; height: 450px; margin: 0 auto; position: relative; transform-style: preserve-3d; transform: rotateY(-25deg) rotateX(0deg);">
+        <!-- Front Cover -->
+        <div class="book-front" style=" left:-40px; position: absolute; width: 100%; height: 100%; background-size: cover; background-position: center; box-shadow: 0 10px 30px rgba(0,0,0,0.1); border-radius: 3px 0 0 3px;" :style="{ backgroundImage: 'url(' + coverImage + ')' }"></div>
+        <!-- Spine (right side) -->
+        <div class="book-spine" style="position: absolute; width: 40px; height: calc(100% - 12px); top: 5px; background: linear-gradient(to right, #d4c4a8 0%, #e8dcc8 50%, #d4c4a8 100%); right: -14px; transform-origin: left; transform: rotateY(-90deg); box-shadow: inset 0 0 15px rgba(0,0,0,0.2);"></div>
+        <!-- Pages -->
+
+        <!-- Parallelogram Shadow -->
+        <div class="book-shadow" style="position: absolute; width: 300px; height: 100px; bottom:-2px; right: -230px; background: linear-gradient(to top, rgba(0, 0, 0, 0.3) 0%, rgba(0, 0, 0, 0.1) 80%, transparent 100%); transform: rotateX(0deg) rotateZ(00deg) skewX(-70deg); transform-origin: left top; filter: blur(15px); opacity: 0.6; z-index: -1;"></div>
+      </div>
+    </div>
     <div v-for="fileName in files" :key="fileName">
       <a :id="fileName.replace('.txt', '')"></a>
       <p v-for="paragraph in paragraphs[fileName]" :key="paragraph">{{ paragraph }}</p>
